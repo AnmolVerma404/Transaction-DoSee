b59b733a11e7b320ff403961c45e787b01d0b177
Anmol Verma <anmolverma102002@gmail.com>
2022-07-15 21:01:41 +0530

    Solidity code and connection hardhar :rocket:

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Transactions {
+    uint256 transactionsCount;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionsCount++;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionsCount;
+    }
+}
